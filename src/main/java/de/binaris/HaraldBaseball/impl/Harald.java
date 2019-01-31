a12f2e05b4da808502c511bcb74b78af18102e8f
Daniel Bleisteiner <daniel.bleisteiner@ecg.de>
2019-01-31 16:38:34 +0100

    Sound wieder raus

--- src/main/java/de/binaris/HaraldBaseball/impl/Harald.java
+++ src/main/java/de/binaris/HaraldBaseball/impl/Harald.java
@@ -1,7 +1,5 @@
 package de.binaris.HaraldBaseball.impl;
 
-import java.io.File;
-
 import de.binaris.HaraldBaseball.errors.IchWeissNichtWoIchBinException;
 import de.binaris.HaraldBaseball.errors.NotOnBaseException;
 import lejos.hardware.Sound;
@@ -21,41 +19,23 @@ public class Harald {
     public void spiele() throws NotOnBaseException, IchWeissNichtWoIchBinException {
         System.out.println("Harald.spiele()");
         
-//        new Thread(new Runnable() {
-//            
-//            @Override
-//            public void run() {
-//                Sound.playSample(new File("/Users/daniel/Desktop/TDD/Last Christmas - Wham.wav"));
-//            }
-//        }).start();
-
         Abschlag abschlag = this.bewusstsein.abschlag(zufall);
         System.out.println("Harald.spiele() : " + abschlag.name());
         
         switch (abschlag) {
             case OKAY:
-                Sound.beep();
                 spielfeld.laufeBases(1);
                 break;
 
             case GUT:
-                Sound.beep();
-                Sound.beep();
                 spielfeld.laufeBases(2);
                 break;
                 
             case BESSER:
-                Sound.beep();
-                Sound.beep();
-                Sound.beep();
                 spielfeld.laufeBases(3);
                 break;
                 
             case HOMERUN:
-                Sound.beep();
-                Sound.beep();
-                Sound.beep();
-                Sound.beep();
                 spielfeld.laufeBases(4);
                 break;
                 
